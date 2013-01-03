22b438bdd0de9d4f4367e790fd130d4967e056df
Yordan Miladinov <yordan@4web.bg>
2013-01-03 02:45:08 +0200

    xmonad: use xterm instead of gnome-terminal

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -23,8 +23,8 @@ import qualified Data.Map        as M
 -- The preferred terminal program, which is used in a binding below and by
 -- certain contrib modules.
 --
--- myTerminal      = "xterm"
-myTerminal      = "gnome-terminal"
+myTerminal      = "xterm"
+-- myTerminal      = "gnome-terminal"
 
 -- Whether focus follows the mouse pointer.
 myFocusFollowsMouse :: Bool
