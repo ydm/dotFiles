f31de6c1247f2f659a0cf5f5b9039031cc23e228
Yordan Miladinov <yordan@4web.bg>
2013-07-21 11:01:50 +0300

    Remove manually downloaded theme packages

--- emacs/themes/get.sh
+++
@@ -1,2 +0,0 @@
-git clone https://github.com/sellout/emacs-color-theme-solarized.git
-git clone https://github.com/chriskempson/tomorrow-theme.git
--- emacs/themes/update.sh
+++
@@ -1,7 +0,0 @@
-cd emacs-color-theme-solarized/
-git pull
-cd ..
-
-cd tomorrow-theme/
-git pull
-cd ..
