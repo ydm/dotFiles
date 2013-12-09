edacc5ebd4bdb450dd89434ffeea3a3c74802b9c
Yordan Miladinov <yordanm@elektrosfera.eu>
2013-12-09 15:36:48 +0200

    Simple implementation of a time-tracking system that should help me tracking my working hours

--- emacs/init/init-defuns.el
+++ emacs/init/init-defuns.el
@@ -84,3 +84,66 @@ buffer."
                 (buffer-list)))
 
 (provide 'init-defuns)
+
+
+;; +--------------+
+;; | Undertime... |
+;; +--------------+
+(defun ut:is-time-string (time-string)
+  (integerp (string-match
+             "^[+-]?[[:digit:]]\\{2\\}:[[:digit:]]\\{2\\}$" time-string)))
+
+(defun ut:current-line (&optional relative)
+  (save-excursion
+    (next-line (or relative 0))
+    (let* ((start (line-beginning-position))
+           (end (line-end-position)))
+      (buffer-substring-no-properties start end))))
+
+(defun ut:minute-value (hours minutes)
+  (+ (* 60 (abs hours))
+     (abs minutes)))
+
+(defun ut:time-to-minutes (time)
+  (if (ut:is-time-string time)
+      (let* ((sign (if (integerp (string-match "^-" time)) -1 1))
+             (parts (split-string time ":"))
+             (hours (string-to-number (car parts)))
+             (minutes (string-to-number (cadr parts))))
+        (message (format "time=%s hours=%d minutes=%d" time hours minutes))
+        (* sign (ut:minute-value hours minutes)))
+    0))
+
+(defun ut:minutes-to-time (minutes)
+  (format "%s%02d:%02d"
+          (if (> 0 minutes) "-" " ")
+          (abs (/ minutes 60))
+          (abs (% minutes 60))))
+
+(defun ut:process-line ()
+  (interactive)
+  (let* ((previous (ut:current-line -1))
+         (weektotal (ut:time-to-minutes (car (last (split-string previous)))))
+         (line (ut:current-line))
+         (parts (split-string line))
+         (arv (ut:time-to-minutes (cadr parts)))
+         (dpt (ut:time-to-minutes (caddr parts)))
+         (total (- dpt arv))
+         (balance (- total (ut:minute-value 8 30)))
+         (weekbalance (+ weektotal balance)))
+    (message (format "weektotal=%s, total=%s, balance=%s, weekbalance=%s"
+                     weektotal total balance weekbalance))
+    (save-excursion
+      (end-of-line)
+      (insert (format " %6s %6s %6s"
+                      (ut:minutes-to-time total)
+                      (ut:minutes-to-time balance)
+                      (ut:minutes-to-time weekbalance))))))
+
+;; #     Date   Arv   Dpt  Total Balnce WBalnc
+;; 02/12/2013 09:50 19:50  10:00  01:30  01:30
+;; 03/12/2013 10:45 17:00
+;; 04/12/2013 10:40 19:00
+;; 05/12/2013 09:25 17:00
+;; 06/12/2013 10:00 17:00
+;; Delete the ";; " at start and invoke (ut:process-line)
