bfdf4565e0d2d360d5a92d74173d7dbf191bae31
Yordan Miladinov <yordan.miladinov@chaosgroup.com>
2015-10-02 11:57:49 +0000

    fix a dsp parsing error

--- emacs/modules/init-dsp.el
+++ emacs/modules/init-dsp.el
@@ -1,12 +1,12 @@
 (require 'cl)
 
 (defun y:dsp-fix-include-path (dir path)
-  "Replaces backslashes with slashes and truncate multiple
+  "Replaces backslashes with slashes and truncates multiple
 slashes to just one.  Also applies Chaos specific stuff:
 - replaces [kK]:\ with proper SDK path
 - replaces maya70 with maya2016"
   (let* ((first (replace-regexp-in-string "\\\\" "/" path))
-	 (second (if (string-prefix-p "k:/" (downcase first))
+	 (second (if (string-prefix-p "k:/" (downcase (substring first 3)))
 		     (concat "~/cgrepo/sdk/linux/" (substring first 3))
 		   first))
 	 (third (replace-regexp-in-string "/+" "/" second))
@@ -15,8 +15,6 @@ slashes to just one.  Also applies Chaos specific stuff:
 		   third))
 	 (fifth (replace-regexp-in-string "maya70" "maya2016" fourth))
 	 (fixed (expand-file-name fifth)))
-    (unless (file-directory-p fixed)
-      (message "Include directory %s doesn't exist" fixed))
     fixed))
 
 (defun y:dsp-read-includes (dsp)
@@ -33,11 +31,15 @@ directories supplied to the compiler"
     res))
 
 (defun y:dsp-read-includes2 (dsp)
-  "Does the same as y:dsp-read-includes, but also fixes
-filepaths"
-  (mapcar (lambda (fn)
-	    (y:dsp-fix-include-path (file-name-directory dsp) fn))
-	  (y:dsp-read-includes dsp)))
+  "Does the same as y:dsp-read-includes, but also checks for
+empty DSP and fixes filepaths"
+  (if (null dsp)
+      '()
+    (mapcar (lambda (fn)
+	      (let ((path (y:dsp-fix-include-path (file-name-directory dsp) fn)))
+		(unless (file-directory-p path)
+		  (message "Include directory %s doesn't exist" path))))
+	    (y:dsp-read-includes dsp))))
 
 (defun y:dsp-search-parents (start)
   "Searches for a *.dsp file.  Works for at most 7 directory
@@ -54,8 +56,8 @@ levels above the current buffer's file"
   (let* ((file (or file (buffer-file-name)))
 	 (dsp (y:dsp-search-parents file))
 	 (dirs (y:dsp-read-includes2 dsp)))
-    (message "Parsing include directories for %s: dsp=%s, #dirs=%d"
-	     file dsp (length dirs))
+    (message "Parsing DSP for include directories: dsp=%s, #dirs=%d"
+	     dsp (length dirs))
     dirs))
 
 (provide 'init-dsp)
