ac4edd17091fceda305ffcd4a9d07742f034fec2
Yordan Miladinov <yordan.miladinov@chaosgroup.com>
2016-01-04 12:23:03 +0000

    stuff

--- emacs/modules/init-company.el
+++ emacs/modules/init-company.el
@@ -1,41 +1,34 @@
-(require 'init-dsp)
-(require 'init-gcc)
-
 (add-to-list '*y:packages* 'company)
-(add-to-list '*y:packages* 'company-c-headers)
-
-(defvar *y:company-clang-hard-coded-flags*
-  '("-DLINUX"
-    "-fpermissive"
-    "-std=c++1y"
-    "-include/usr/include/GL/gl.h"
-    "-I/usr/include/qt4"))
 
 (eval-after-load 'company
   (lambda ()
-    (delete 'comapny-cmake company-backends)
-    (delete 'company-bbdb company-backends)
-    (delete 'company-css company-backends)
-    (delete 'company-dabbrev company-backends)
-    (delete 'company-eclim company-backends)
-    (delete 'company-nxml company-backends)
-    (delete 'company-oddmuse company-backends)
-    (delete 'company-semantic company-backends)
-    (delete 'company-xcode company-backends)
-    (add-to-list 'company-backends 'company-c-headers)))
-
-(defun y:company-set-include-paths ()
-  (let* ((syst (y:gcc-system-include-dirs "clang"))
-	 (user (y:dsp-parse-includes-for-file))
-	 (dirs (append syst user)))
-    (setq company-c-headers-path-system syst)
-    (setq company-c-headers-path-user user)
-    (setq company-clang-arguments
-	  (append
-	   (mapcar (lambda (d) (concat "-I" d)) dirs)
-	   *y:company-clang-hard-coded-flags*))))
+    (setq company-backends '(company-files))))
 
 (add-hook 'after-init-hook (lambda () (global-company-mode 1)))
-(add-hook 'c++-mode-hook #'y:company-set-include-paths)
+
+;; (require 'init-dsp)
+;; (require 'init-gcc)
+
+;; (add-to-list '*y:packages* 'company-c-headers)
+
+;; (defvar *y:company-clang-hard-coded-flags*
+;;   '("-DLINUX"
+;;     "-fpermissive"
+;;     "-std=c++1y"
+;;     "-include/usr/include/GL/gl.h"
+;;     "-I/usr/include/qt4"))
+
+;; (defun y:company-set-include-paths ()
+;;   (let* ((syst (y:gcc-system-include-dirs "clang"))
+;; 	 (user (y:dsp-parse-includes-for-file))
+;; 	 (dirs (append syst user)))
+;;     (setq company-c-headers-path-system syst)
+;;     (setq company-c-headers-path-user user)
+;;     (setq company-clang-arguments
+;; 	  (append
+;; 	   (mapcar (lambda (d) (concat "-I" d)) dirs)
+;; 	   *y:company-clang-hard-coded-flags*))))
+
+;; (add-hook 'c++-mode-hook #'y:company-set-include-paths)
 
 (provide 'init-company)
