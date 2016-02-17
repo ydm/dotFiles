6c75284f0aeb2260d322efaf6aca9e35f6fa2388
Yordan Miladinov <yordan@4web.bg>
2016-02-17 11:00:41 +0200

    add cmake mode

--- emacs/modules/init-modes.el
+++ emacs/modules/init-modes.el
@@ -1,4 +1,5 @@
-(add-to-list '*y:packages* '(init-modes ag))
+;; (add-to-list '*y:packages* '(init-modes ag))
+(add-to-list '*y:packages* '(init-modes cmake-mode))
 (add-to-list '*y:packages* '(init-modes glsl-mode))
 (add-to-list '*y:packages* '(init-modes haskell-mode))
 
