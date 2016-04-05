7b8b706392f8298b5543f67b076288887d585622
Yordan Miladinov <yordan.miladinov@chaosgroup.com>
2016-04-05 19:55:02 +0000

    stuff

--- emacs/modules/init-yasnippet/init-yasnippet.el
+++ emacs/modules/init-yasnippet/init-yasnippet.el
@@ -1,1 +1,11 @@
+(y:install-packages yasnippet)
+
+(setq *y:init-yasnippet/file-name* load-file-name)
+
+(eval-after-load 'yasnippet
+  (lambda ()
+    (let* ((dir (file-name-directory *y:init-yasnippet/file-name*))
+	   (snippets (concat dir "snippets")))
+      (add-to-list 'yas-snippet-dirs snippets))))
+
 (provide 'init-yasnippet)
