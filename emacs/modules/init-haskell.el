19d7609b898fd5362d896916d8e58a082ed0a56e
Yordan Miladinov <yordan.miladinov@chaosgroup.com>
2016-04-05 20:37:36 +0000

    what about this?

--- emacs/modules/init-haskell.el
+++ emacs/modules/init-haskell.el
@@ -1,3 +1,2 @@
 (y:install-packages haskell-mode)
-(add-hook 'haskell-mode-hook (lambda () (subword-mode 1)))
 (provide 'init-haskell)
--- emacs/modules/init-mel/init-mel.el
+++ emacs/modules/init-mel/init-mel.el
@@ -3,9 +3,6 @@
 
 (add-hook 'mel-mode-hook
 	  (lambda ()
-	    ;; 1. Subword mode
-	    (subword-mode)
-	    ;; 2. E2M
 	    (require 'e2m)
 	    (local-set-key (kbd "C-c C-c") #'e2m/send-buffer)
 	    (local-set-key (kbd "C-c C-q") #'e2m/send-quick)
