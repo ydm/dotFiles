b007ed8aa74fb0a7b4fb578145c72927d8885826
Yordan Miladinov <yordan.miladinov@chaosgroup.com>
2016-06-06 13:29:38 +0000

    untab

--- emacs/modules/init-modes.el
+++ emacs/modules/init-modes.el
@@ -1,12 +1,12 @@
 (y:install-packages cmake-mode glsl-mode yaml-mode)
 
 (add-hook 'emacs-lisp-mode-hook
-	  (lambda ()
-	    (push '("/=" . ?≠) prettify-symbols-alist)
-	    (push '("<=" . ?≤) prettify-symbols-alist)
-	    (push '(">=" . ?≥) prettify-symbols-alist)
-	    (push '("function" . ?ƒ) prettify-symbols-alist)
-	    (push '("nil" . ?∅) prettify-symbols-alist)))
+          (lambda ()
+            (push '("/=" . ?≠) prettify-symbols-alist)
+            (push '("<=" . ?≤) prettify-symbols-alist)
+            (push '(">=" . ?≥) prettify-symbols-alist)
+            (push '("function" . ?ƒ) prettify-symbols-alist)
+            (push '("nil" . ?∅) prettify-symbols-alist)))
 
 (custom-set-variables
  '(global-prettify-symbols-mode t))
