5c817f271222fc83453bd9df24a84225d3a0c6e5
Yordan Miladinov <yordan.miladinov@chaosgroup.com>
2016-09-20 11:36:13 +0000

    stuff

--- emacs/modules/init-chaos.el
+++ emacs/modules/init-chaos.el
@@ -3,7 +3,8 @@
   (interactive)
   (let* ((vray "/usr/autodesk/maya2016/vray/bin/vray")
 	 (scene (buffer-file-name))
-	 (cmd (format "%s -sceneFile=%s -imgFile=/tmp/output.png &" vray scene)))
+         (cmd (format "%s -sceneFile=%s &" vray scene)))
+	 ;; (cmd (format "%s -sceneFile=%s -imgFile=/tmp/output.png &" vray scene)))
     (shell-command cmd)))
 
 (provide 'init-chaos)
--- emacs/modules/init-yasnippet/init-yasnippet.el
+++ emacs/modules/init-yasnippet/init-yasnippet.el
@@ -2,12 +2,12 @@
 
 (setq *y:init-yasnippet/file-name* load-file-name)
 
-(add-hook 'after-init-hook
-	  (lambda ()
-            (require 'yasnippet)
-	    (let* ((dir (file-name-directory *y:init-yasnippet/file-name*))
-		   (snippets (concat dir "snippets")))
-	      (add-to-list 'yas-snippet-dirs snippets))
-            (yas-global-mode 1)))
+;; (add-hook 'after-init-hook
+;; 	  (lambda ()
+;;             (require 'yasnippet)
+;; 	    (let* ((dir (file-name-directory *y:init-yasnippet/file-name*))
+;; 		   (snippets (concat dir "snippets")))
+;; 	      (add-to-list 'yas-snippet-dirs snippets))
+;;             (yas-global-mode 1)))
 
 (provide 'init-yasnippet)
