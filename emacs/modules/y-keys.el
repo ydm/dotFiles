d4b153be4d0727a2db6bd2fe68f28b6a7f7cd3eb
Yordan Miladinov <jordanmiladinov@gmail.com>
2019-07-20 09:30:48 +0300

    cosmetics

--- emacs/modules/y-keys.el
+++ emacs/modules/y-keys.el
@@ -20,22 +20,22 @@ Otherwise fallback to (ido-find-file)."
    (indent-for-tab-command))
 
  ;; Unset system keys
- (global-unset-key (kbd   "M-x"))	; execute-extend-command
  (global-unset-key (kbd "C-o"))         ; open-line
  (global-unset-key (kbd "C-x C-b"))     ; list-buffers
  (global-unset-key (kbd "C-x C-f"))     ; find-file
  (global-unset-key (kbd "C-x o"))       ; other-frame
  (global-unset-key (kbd "C-z"))         ; suspend-frame
+ (global-unset-key (kbd "M-x"))         ; execute-extend-command
 
  ;; Reuse system keys
- (global-set-key (kbd   "M-x") #'smex)
- (global-set-key (kbd "C-o") #'y:keys/open-line)
+ (global-set-key (kbd "C-o")     #'y:keys/open-line)
  (global-set-key (kbd "C-x C-b") #'bs-show)
  (global-set-key (kbd "C-x C-f") #'y:find-file)
- 
+ (global-set-key (kbd "M-x")     #'smex)
+
  ;; New global keys
- (global-set-key (kbd "<f13>") #'smex)
- (global-set-key (kbd "C-c i") #'y:ido-imenu)
- (global-set-key (kbd "s-o") #'forward-whitespace))
+ (global-set-key (kbd "<f13>")   #'smex)
+ (global-set-key (kbd "C-c i")   #'y:ido-imenu)
+ (global-set-key (kbd "s-o")     #'forward-whitespace))
 
 (provide 'y-keys)
