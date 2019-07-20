3a6c0dea5d1e67aec4ec886fa9a8dfbfb664439a
Yordan Miladinov <jordanmiladinov@gmail.com>
2019-07-20 22:35:04 +0300

    some sweet new keybindings:
    - (wrap-in-parentheses) on C-(
    - (revisit-with-sudo) on C-x C-r

--- emacs/modules/y-keys.el
+++ emacs/modules/y-keys.el
@@ -1,41 +1,30 @@
 (y:module
- '(find-file-in-project smex)
+ '(smex)
 
- (require 'find-file-in-project)         ; find-file-in-project
- (require 'y-interactive)                ; y:ido-imenu
- (require 'y-lib)                        ; y:project-root
-
- (defun y:find-file (prefix)
-   "If there's a project root, use (find-file-in-project).
-Otherwise fallback to (ido-find-file)."
-   (interactive "P")
-   (if (and (null prefix) (y:project-root))
-       (find-file-in-project)
-     (ido-find-file)))
-
- (defun y:keys/open-line ()
-   (interactive)
-   (move-beginning-of-line nil)
-   (open-line 1)
-   (indent-for-tab-command))
+ (require 'y-interactive)
 
  ;; Unset system keys
- (global-unset-key (kbd "C-o"))         ; open-line
- (global-unset-key (kbd "C-x C-b"))     ; list-buffers
- (global-unset-key (kbd "C-x C-f"))     ; find-file
- (global-unset-key (kbd "C-x o"))       ; other-frame
- (global-unset-key (kbd "C-z"))         ; suspend-frame
- (global-unset-key (kbd "M-x"))         ; execute-extend-command
+ (global-unset-key (kbd "C-o"))          ; open-line
+ (global-unset-key (kbd "C-x C-b"))      ; list-buffers
+ (global-unset-key (kbd "C-x C-f"))      ; find-file
+ (global-unset-key (kbd "C-x o"))        ; other-frame
+ (global-unset-key (kbd "C-z"))          ; suspend-frame
+ (global-unset-key (kbd "M-x"))          ; execute-extend-command
+ (global-unset-key (kbd "C-x C-r"))      ; find-file-read-only
 
  ;; Reuse system keys
- (global-set-key (kbd "C-o")     #'y:keys/open-line)
- (global-set-key (kbd "C-x C-b") #'bs-show)
- (global-set-key (kbd "C-x C-f") #'y:find-file)
- (global-set-key (kbd "M-x")     #'smex)
+ (global-set-key (kbd "C-o")             #'y:open-line)
+ (global-set-key (kbd "C-x C-b")         #'bs-show)
+ (global-set-key (kbd "C-x C-f")         #'y:find-file)
+ (global-set-key (kbd "C-x C-r")         #'y:revisit-with-sudo)
+ (global-set-key (kbd "M-x")             #'smex)
 
- ;; New global keys
- (global-set-key (kbd "<f13>")   #'smex)
- (global-set-key (kbd "C-c i")   #'y:ido-imenu)
- (global-set-key (kbd "s-o")     #'forward-whitespace))
+ ;; New keys
+ (define-key prog-mode-map (kbd "C-(")   #'y:wrap-in-parentheses)
+ (define-key prog-mode-map (kbd "C-c i") #'y:ido-imenu)
+ (define-key prog-mode-map (kbd "s-o")   #'forward-whitespace)
+ (global-set-key (kbd "<f13>")           #'smex)
+
+ )
 
 (provide 'y-keys)
