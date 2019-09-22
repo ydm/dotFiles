30adf3fedbea0372ee3d0a1c80e57c454e67eaa6
Yordan Miladinov <jordanmiladinov@gmail.com>
2019-09-22 12:15:10 +0300

    initial irony config

---
+++ emacs/modules/y-irony.el
@@ -0,0 +1,13 @@
+;; (add-hook 'c++-mode-hook 'irony-mode)
+;; (add-hook 'c-mode-hook 'irony-mode)
+;; (add-hook 'objc-mode-hook 'irony-mode)
+
+;; (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
+;; (eval-after-load 'flycheck
+;;   '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
+
+;; (add-hook 'c++-mode-hook 
+;;           (lambda ()
+;;             (define-key c++-mode-map (kbd "C-.") #'completion-at-point))
+;;           (flycheck-mode 1))
