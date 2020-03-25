628dcc87ac098df5cbf57c59070a4705192408e0
Yordan Miladinov <yordan.miladinov@flolive.net>
2020-03-25 23:17:45 +0200

    inherit function face for which-function-mode

--- emacs/modules/y-theme.el
+++ emacs/modules/y-theme.el
@@ -1,9 +1,21 @@
 (y:module
- '((packages atom-dark)
+ '((packages atom-dark-theme)
    (hookvar . y-theme-hook))
 
  (mapcar #'disable-theme custom-enabled-themes)
- (load-theme 'atom-dark t)
- )
+ (load-theme 'atom-dark t))
+
+;; Once my theme is loaded, this hook gets fired (see y:module).  It
+;; invokes (custom-theme-set-faces) for the currently enabled theme
+;; and modifies the `which-func' face by making it inherit the
+;; `font-lock-function-name-face' property of the current theme.  This
+;; way (which-function-mode) doesn't try to guess (for good) what
+;; color the face should be.
+(add-hook 'y-theme-hook
+          (lambda ()
+            (custom-theme-set-faces
+             (car custom-enabled-themes)
+             '(which-func ((t :inherit font-lock-function-name-face
+                              :slant normal))))))
 
 (provide 'y-theme)
