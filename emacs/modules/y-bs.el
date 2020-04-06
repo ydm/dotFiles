91756ab04f1cd5238b48bdb7ed2385a171ee72f5
Yordan Miladinov <yordan.miladinov@flolive.net>
2020-04-06 14:15:05 +0300

    bs stuff

--- emacs/modules/y-bs.el
+++ emacs/modules/y-bs.el
@@ -4,18 +4,22 @@
   "\
 Returns t for buffer that should be killed.
 
+TODO: SPECIFY CONDITIONS BETTER, THE LIST ISN'T ACCURATE
+
 Conditions are:
-  1. Buffer starts with star;
-  2. Buffer is neither one of *Messages*, *Packages* or *scratch*;
-  3. Buffer doesn't have an associated process running."
+  1. Buffer starts with star
+  2. Buffer is neither one of *Messages*, *Packages* or *scratch*
+  3. Buffer doesn't have an associated process running
+  4. Buffer is not int dired-mode
+"
   (let ((mode (with-current-buffer b major-mode))
         (name (buffer-name b))
         (protected '("*Messages*" "*Packages*" "*scratch*")))
     (cond ((eq b (current-buffer)) nil)
-          ((eq mode 'dired-mode) t)
+          ((eq mode 'dired-mode) nil)
           ((and (string-prefix-p "*" name)
                 (not (member name protected))
-                (null (get-buffer-process b))) t)
+                (null (get-buffer-process b))))
           (t nil))))
 
 (defadvice bs-show (before y:bs-kill-system-buffers-before-bs-show activate)
