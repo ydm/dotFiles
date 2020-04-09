4edcbdc632192d06a18a2067fcaeba51e7593518
Yordan Miladinov <yordan.miladinov@flolive.net>
2020-04-10 02:45:45 +0300

    better sway config, still not done yet

---
+++ sway/gen.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import json
+from subprocess import check_output
+
+
+def kb_config(d):
+    ident = d['identifier']
+    s = (f'input "{ident}" {{\n'
+         f'    xkb_options ctrl:nocaps\n'
+         f'}}')
+    return s
+
+
+def touchpad_config(d):
+    ident = d['identifier']
+    s = (f'input "{ident}" {{\n'
+         f'    tap enabled\n'
+         f'}}')
+    return s
+
+
+def main():
+    out = check_output(['swaymsg', '-t', 'get_inputs'])
+    data = json.loads(out)
+    for d in data:
+        ident = d['identifier'].lower()
+        if d['type'] == 'keyboard' and 'keyboard' in ident:
+            print(kb_config(d))
+        elif d['type'] == 'touchpad' and 'touchpad' in ident:
+            print(touchpad_config(d))
+
+
+if __name__ == '__main__':
+    main()
