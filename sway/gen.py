182508f141b00dabffaf3884c296581b3b24bf8e
Yordan Miladinov <jordanmiladinov@gmail.com>
2020-04-22 11:54:36 +0300

    broader nocaps config

--- sway/gen.py
+++ sway/gen.py
@@ -25,7 +25,11 @@ def main():
     data = json.loads(out)
     for d in data:
         ident = d['identifier'].lower()
-        if d['type'] == 'keyboard' and 'keyboard' in ident:
+        if (d['type'] == 'keyboard'
+            and ('keyboard' in ident
+                 or 'kb' in ident)):
+            # Or should we ask which keyboards to include and which
+            # not to?
             print(kb_config(d))
         elif d['type'] == 'touchpad' and 'touchpad' in ident:
             print(touchpad_config(d))
