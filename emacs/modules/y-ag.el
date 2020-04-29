73903d0d7c722c0dc78438cb07dee820ec7e799c
Yordan Miladinov <yordan.miladinov@flolive.net>
2020-04-29 17:09:49 +0300

    new module y-ag

---
+++ emacs/modules/y-ag.el
@@ -0,0 +1,14 @@
+(y:module
+ '((packages ag))
+
+ (require 'ag)
+ (define-key ag-mode-map (kbd "q")
+   (lambda (prefix)
+     (interactive "P")
+     (if prefix
+         (kill-buffer-and-window)
+       (kill-buffer))))
+ (require 'dired)
+ (define-key dired-mode-map (kbd "/") #'ag))
+
+(provide 'y-ag)
