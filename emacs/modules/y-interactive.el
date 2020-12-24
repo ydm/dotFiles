891605bab737f0c753bf393e0d8f209ef6b239ba
Yordan Miladinov <yordan.miladinov@flolive.net>
2020-12-24 20:57:36 +0200

    add toggle-window-split

--- emacs/modules/y-interactive.el
+++ emacs/modules/y-interactive.el
@@ -127,6 +127,32 @@ If there is a prefix argument, ask the user for a file to visit."
 ;;         (find-alternate-file (format "/sudo:root@localhost:%s" x))
 ;;       (user-error "This buffer is not visiting a file"))))
 
+(defun y:toggle-window-split ()
+  "https://github.com/magnars/.emacs.d/ ... /defuns/buffer-defuns.el#L42"
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+             (next-win-buffer (window-buffer (next-window)))
+             (this-win-edges (window-edges (selected-window)))
+             (next-win-edges (window-edges (next-window)))
+             (this-win-2nd (not (and (<= (car this-win-edges)
+                                         (car next-win-edges))
+                                     (<= (cadr this-win-edges)
+                                         (cadr next-win-edges)))))
+             (splitter
+              (if (= (car this-win-edges)
+                     (car (window-edges (next-window))))
+                  'split-window-horizontally
+                'split-window-vertically)))
+        (delete-other-windows)
+        (let ((first-win (selected-window)))
+          (funcall splitter)
+          (if this-win-2nd (other-window 1))
+          (set-window-buffer (selected-window) this-win-buffer)
+          (set-window-buffer (next-window) next-win-buffer)
+          (select-window first-win)
+          (if this-win-2nd (other-window 1))))))
+
 (defun y:wrap-in-parentheses ()
   (interactive)
   (insert-char ?\()
