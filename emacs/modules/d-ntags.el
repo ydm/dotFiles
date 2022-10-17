6a7fac51d888f2f14fe43fbe52341f6edfe6cb25
Yordan Miladinov <yordanm@pm.me>
2022-10-17 14:34:54 +0300

    emacs/modules/ntags: adapt

--- emacs/modules/d-ntags.el
+++ emacs/modules/d-ntags.el
@@ -8,8 +8,15 @@
    (d:load-library "interactives")
    (if-let* ((root (d:project-root))
              (thing (thing-at-point 'symbol))
+             (current-file (buffer-file-name))
              (update (if prefix "--update" ""))
-             (command (format "/home/d/dotFiles/wannabin/ntagsserver.py %s %s --tag %s" update root thing))
+             (command
+              (format
+               "/home/d/dotFiles/wannabin/ntagsserver.py --current-file %s %s --tag %s %s"
+               current-file
+               update
+               thing
+               root))
              (output (shell-command-to-string command))
              (parts (split-string output ":"))
              (filename (car parts))
@@ -17,11 +24,11 @@
              (lineno (string-to-number line)))
        (progn (find-file filename)
               (goto-char (save-mark-and-excursion
-                           (goto-line (1+ lineno))
+                           (goto-line lineno)
                            (back-to-indentation)
                            (point)))
               (recenter-top-bottom)
-              (and prefix (message "[NTAGS] command=%s" command)))
+              (and prefix (message "[NTAGS] Command: %s" command)))
      (message "[NTAGS] Could not find definition: root=%s thing=%s command=%s output=%s parts=%s filename=%s line=%s"
               root thing command output parts filename line)))
 
