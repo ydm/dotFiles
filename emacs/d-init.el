e9132a26b41a3e591479050c686a4a6d76958941
Yordan Miladinov <yordanm@pm.me>
2022-11-11 12:12:16 +0200

    d-init.el: reorganize

--- emacs/d-init.el
+++ emacs/d-init.el
@@ -18,14 +18,41 @@
  '(eglot-highlight-symbol-face ((t (:background "gray23")))))
 
 (custom-set-variables
+ ;; Pacakges
+ '(package-archives
+   '(("melpa" . "http://melpa.org/packages/")
+     ("gnu" . "https://elpa.gnu.org/packages/")))
+
+ ;; Misc
  '(backup-directory-alist '(("." . "~/.emacs.d/backup")))
  '(calendar-week-start-day 1)
- '(column-number-mode t)
- '(company-tooltip-align-annotations t)
  '(compilation-always-kill t)
  '(compilation-scroll-output 'first-error)
  '(default-input-method "bulgarian-alt-phonetic")
  '(delete-selection-mode t)
+ '(gc-cons-threshold 33554432)
+ '(gdb-many-windows t)
+ '(highlight-indent-guides-method 'column)
+ '(hs-hide-comments-when-hiding-all nil)
+ '(image-dired-thumb-size 150)
+ '(indent-tabs-mode nil)
+ '(inhibit-startup-screen t)
+ '(mouse-yank-at-point t)
+ '(safe-local-variable-values '((git-commit-major-mode . git-commit-elisp-text-mode)))
+ '(scroll-error-top-bottom t)
+ '(scroll-preserve-screen-position t)
+ '(show-paren-mode t)
+ '(truncate-lines t)
+ '(vterm-max-scrollback 8192)
+
+ ;; Company.
+ '(company-dabbrev-code-ignore-case t)
+ '(company-dabbrev-ignore-case t)
+ '(company-etags-ignore-case t)
+ '(company-keywords-ignore-case t)
+ '(company-tooltip-align-annotations t)
+
+ ;; Dired.
  '(dired-auto-revert-buffer t)
  '(dired-create-destination-dirs 'ask)
  '(dired-dwim-target t)
@@ -33,32 +60,24 @@
  '(dired-listing-switches "-alh")
  '(dired-recursive-copies 'always)
  '(dired-recursive-deletes 'top)
- '(gc-cons-threshold 1600000)
- '(gdb-many-windows t)
+
+ ;; Global minor modes.
+ '(column-number-mode t)
  '(global-display-line-numbers-mode t)
  '(global-subword-mode t)
- '(highlight-indent-guides-method 'column)
- '(hs-hide-comments-when-hiding-all nil)
+ '(popwin-mode t)
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil)
+ '(yas-global-mode t)
+
+ ;; Ido
  '(ido-enable-flex-matching t)
  '(ido-mode 'both nil (ido))
- '(image-dired-thumb-size 150)
- '(indent-tabs-mode nil)
- '(inhibit-startup-screen t)
+
+ ;; Markdown
  '(markdown-preview-http-port 9268)
- ;; '(markdown-preview-stylesheets
- ;;   '("https://thomasf.github.io/solarized-css/solarized-dark.min.css"))
- '(mouse-yank-at-point t)
- '(package-archives
-   '(("melpa" . "http://melpa.org/packages/")
-     ("gnu" . "https://elpa.gnu.org/packages/")))
- '(safe-local-variable-values '((git-commit-major-mode . git-commit-elisp-text-mode)))
- '(scroll-bar-mode nil)
- '(scroll-error-top-bottom t)
- '(scroll-preserve-screen-position t)
- '(show-paren-mode t)
- '(tool-bar-mode nil)
- '(truncate-lines t)
- '(yas-global-mode t))
+ '(markdown-preview-stylesheets
+   '("https://thomasf.github.io/solarized-css/solarized-dark.min.css")))
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 (put 'dired-find-alternate-file 'disabled nil)
