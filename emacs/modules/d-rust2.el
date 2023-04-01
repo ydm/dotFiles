07051e077f7ebd65a84968eae3eddbf5b2c8d661
Yordan Miladinov <ymiladinov@blockdaemon.com>
2023-04-01 12:13:48 +0300

    initially add the d-rust2.el module, where i configure tree-sitter and other goodies. should combine that with the existing d-rust.el

--- emacs/modules/d-rust2.el
+++ emacs/modules/d-rust2.el
@@ -3,11 +3,16 @@
 (d:module
  '((packages rust-mode))
 
- (add-hook 'rust-mode-hook #'eglot-ensure)
+ (add-hook 'rust-mode-hook    #'rust-ts-mode)
  (add-hook 'rust-ts-mode-hook #'eglot-ensure)
 
- (with-eval-after-load 'eglot
-   (add-to-list 'eglot-server-programs
-                '((rust-ts-mode rust-mode) . ("/home/d/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin/rust-analyzer")))))
+ ;; https://joaotavora.github.io/eglot/
+ (let* ((path "~/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin/rust-analyzer")
+        (full (expand-file-name path))
+        (key '(rust-ts-mode rust-mode)))
+   (message "full=%s" full)
+   (with-eval-after-load 'eglot
+     (setq eglot-server-programs (assoc-delete-all key eglot-server-programs))
+     (add-to-list 'eglot-server-programs (list key full)))))
 
 (provide 'd-rust2)
