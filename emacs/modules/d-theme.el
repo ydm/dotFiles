f9447175772a1b0df62ce3803d48d4eb594403b5
Yordan Miladinov <jordanmiladinov@gmail.bg>
2023-04-27 09:58:36 +0300

    ops

--- emacs/modules/d-theme.el
+++ emacs/modules/d-theme.el
@@ -1,7 +1,7 @@
 ;; -*- lexical-binding: t -*-
 
 (use-package catppuccin-theme
-  :init
+  :config
   (when (display-graphic-p)
     (mapcar #'disable-theme custom-enabled-themes)
     (load-theme 'catppuccin t))
