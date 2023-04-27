30d982d21ec8c633c2934b08810ddd4cfb3db323
Yordan Miladinov <jordanmiladinov@gmail.bg>
2023-04-27 09:58:32 +0300

    yeah...

--- emacs/modules/d-packages.el
+++ emacs/modules/d-packages.el
@@ -12,7 +12,7 @@
 (use-package eldoc-box
   :init (add-hook 'eldoc-mode-hook #'eldoc-box-hover-mode)
   :load-path "~/dev/emacs/eldoc-box"
-  :custom ((eldoc-box-minor-mode-string " eb")
+  :custom ((eldoc-box-lighter " eb")
            (eldoc-box-offset '(16 24 16)))
   :custom-face (eldoc-box-border ((t (:background "#454545")))))
 
