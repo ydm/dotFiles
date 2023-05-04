b589d9b7db3a92525baec3555f5f2573603716e5
Yordan Miladinov <ymiladinov@blockdaemon.com>
2023-05-04 15:38:22 +0300

    add d-nim

--- emacs/modules/d-nim.el
+++ emacs/modules/d-nim.el
@@ -1,3 +1,15 @@
 ;; TODO: '(highlight-indent-guides-method 'column)
 
+(use-package nim-mode
+  :config
+  (add-hook 'nim-mode-hook
+            (lambda ()
+              (eldoc-mode -1)
+              (which-function-mode 1)))
+  :ensure t)
+
+(use-package highlight-indent-guides
+  :hook nim-mode
+  :ensure t)
+
 (provide 'd-nim)
