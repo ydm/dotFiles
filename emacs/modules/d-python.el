5dd62ea2e2bffc1d1af49fdb28ea4575c55b6b59
Yordan Miladinov <yordanm@proton.me>
2025-09-05 10:40:31 +0300

    use pyright-langserver for python

--- emacs/modules/d-python.el
+++ emacs/modules/d-python.el
@@ -1,5 +1,11 @@
 ;; -*- lexical-binding: t -*-
 
+(with-eval-after-load 'eglot
+  (add-to-list 'eglot-server-programs
+               '(python-mode . ("pyright-langserver" "--stdio")))
+  (add-to-list 'eglot-server-programs
+               '(python-ts-mode . ("pyright-langserver" "--stdio"))))
+
 (use-package python
   :init
   (add-hook 'python-mode-hook    #'python-ts-mode)
